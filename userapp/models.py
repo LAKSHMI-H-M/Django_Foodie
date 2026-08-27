114eba1def447e826a7cf2212660d4531e26b9f9
Lakshmi H M <lakshmimudagolmodagol@gmail.com>
2026-08-27 16:36:28 +0530

    Added signup login and worked on database

---
+++ userapp/models.py
@@ -0,0 +1,58 @@
+from django.db import models
+
+class User(models.Model):
+  name = models.CharField(max_length=200)
+  email = models.EmailField(unique=True)
+  mobile = models.PositiveBigIntegerField()
+  password = models.CharField(max_length=200)
+  
+  def __str__(self):
+    return self.name
+
+class UserProfile(models.Model):
+  user = models.OneToOneField(User,on_delete=models.CASCADE)
+  bio = models.TextField(help_text="Enter Bio")
+  landmark = models.CharField(max_length=200)
+  city = models.CharField(max_length=100)
+  state = models.CharField(max_length=100)
+  pincode = models.CharField(max_length=50)
+  address = models.TextField(help_text="Enter address")
+  latitude = models.DecimalField(max_digits=10, decimal_places=7)
+  longitude = models.DecimalField(max_digits=10, decimal_places=7)
+  profilepic = models.ImageField(upload_to="users/")
+  
+  def __str__(self):
+    return f"Profile of {self.user.name}"
+  
+class Restaurant(models.Model):
+  name = models.CharField(max_length=200)
+  ownername = models.CharField(max_length=200)
+  mobile = models.PositiveBigIntegerField()
+  email = models.EmailField(unique=True)
+  address = models.TextField(help_text="Enter address")
+  latitude = models.DecimalField(max_digits=10,decimal_places=7)
+  longitude = models.DecimalField(max_digits=10, decimal_places=7)
+  restaurantpic = models.ImageField(upload_to="restaurants/")
+  is_active = models.BooleanField(default=True)
+  
+  def __str__(self):
+    return self.name
+  
+class Category(models.Model):
+  name = models.CharField(max_length=200)
+  
+  def __str__(self):
+    return self.name
+  
+class Food(models.Model):
+  restaurant = models.ForeignKey(Restaurant,on_delete=models.CASCADE)
+  category = models.ForeignKey(Category,on_delete=models.CASCADE)
+  name = models.CharField(max_length=200)
+  description = models.TextField()
+  price = models.DecimalField(max_digits=8,decimal_places=2)
+  foodimage = models.ImageField(upload_to="foods/")
+  isavailable = models.BooleanField(default=True)
+  
+  def __str__(self):
+    return self.name
+  
---
+++ userapp/views.py
@@ -0,0 +1,42 @@
+from django.shortcuts import render,redirect
+from django.http import HttpResponse
+from userapp.models import User
+
+def signup(request):
+  if request.method=="POST":
+    name = request.POST.get("a")
+    email = request.POST.get("b")
+    mobile = request.POST.get("c")
+    password = request.POST.get("d")
+    
+    User.objects.create(
+      name = name,
+      email = email,
+      mobile = mobile,
+      password=password
+    )
+    return redirect("login_link")
+  return render(request,"userapp/signup.html")
+
+
+def login(request):
+  if request.method=="POST":
+    email = request.POST.get("a")
+    password = request.POST.get("b")
+    
+    try:
+      user = User.objects.get(email=email)
+      if user.email==email and user.password==password:
+        request.session["USERID"] = user.id
+        request.session["EMAIL"] = user.email
+        request.session["NAME"] = user.name
+        return HttpResponse("Login success")
+      else:
+        return HttpResponse("Invalid Credentials")
+    except User.DoesNotExist:
+      return HttpResponse("First Register Account")
+    except User.MultipleObjectsReturned:
+      return HttpResponse("Multiple Data Found")
+  return render(request,"userapp/login.html")
+    
+    
